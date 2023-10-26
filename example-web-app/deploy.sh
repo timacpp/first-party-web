77a0c8e32e11641a424dff4735061b08a102ae88
tvedmedenko <tymofii.vedmedenko@rtbhouse.com>
2023-10-26 08:18:38 +0200

    Use official mysql docker image

--- example-web-app/deploy.sh
+++ example-web-app/deploy.sh
@@ -4,7 +4,7 @@ set -ox pipefail
 
 if [[ $1 == mysql || -z $1 ]]; then
   docker stop books-mysql > /dev/null 2>&1
-  docker run --name books-mysql --network=host --rm -di -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=books -e MYSQL_ROOT_HOST=% mysql/mysql-server:latest
+  docker run --name books-mysql --network=host --rm -di -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=books -e MYSQL_ROOT_HOST=% mysql:latest
 fi
 
 if [[ $1 == app || -z $1 ]]; then
